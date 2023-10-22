fa8b8fbb7d3aced3bef38df710e6e46ee20287ec
rida <ridarafiq54@gmail.com>
2023-10-23 00:27:31 +0400

    edited index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const sql = require("mssql");
+// const sql = require("mssql");
+var mysql = require('mysql');
 const app = express();
 
 app.use(cors());
@@ -31,7 +32,8 @@ router.get( "/",
 
 const sqlInfo = {
   mysql: {
-      host: '127.0.0.1',
+      host: '54.164.166.238',
+      port: '3306',
       database: 'ReactProducts',
       user: 'rida',
       password: 'rida@123',
@@ -44,45 +46,117 @@ const sqlInfo = {
 
 // Default Route
 
+// router.get(
+//     "/:table",
+//     async (req, res, next) => {
+//       let pool = await sql.connect(sqlInfo.sql);
+
+//       var Result;
+//       try {
+//         switch (req.params.table.toLowerCase()) {
+//           case "products":
+//             Result = await pool.request()
+//             .query("SELECT * FROM products");
+
+
+//            let result = await Promise.all(
+//               Result.recordset.map(async(item)=>{
+//                 let images = await pool.request()
+//                  .query("SELECT url FROM image where prd_id = " + item.id );
+//                  item.images=[];
+//                  let imagesFormat = images.recordset?.map((ele)=>{
+//                    return ele?.url;
+//                  }) ?? [];
+//                  item["images"] = imagesFormat;
+//                  return item;
+//                })
+//             );
+
+//            res.send(result);
+//             break;
+//           default: {
+//             Result = await pool.request()
+//               .query("SELECT * FROM products JOIN image ON products.id = image.prd_id");
+//             res.send(Result.recordset);
+//           }
+//         }
+//       } catch (ex) {
+//         res.send(ex);
+//       }
+//     }
+//   );
+
+
+// Default Route
+
 router.get(
-    "/:table",
-    async (req, res, next) => {
-      let pool = await sql.connect(sqlInfo.sql);
+  "/:table",
+  async (req, res, next) => {
+    let connection = mysql.createConnection({
+      host: '172.17.0.1',
+      port: '3306',
+      database: 'ReactProducts',
+      user: 'rida',
+      password: 'rida@123',
+    });
 
-      var Result;
-      try {
-        switch (req.params.table.toLowerCase()) {
-          case "products":
-            Result = await pool.request()
-            .query("SELECT * FROM products");
+    connection.connect();
 
+    var Result;
 
-           let result = await Promise.all(
-              Result.recordset.map(async(item)=>{
-                let images = await pool.request()
-                 .query("SELECT url FROM image where prd_id = " + item.id );
-                 item.images=[];
-                 let imagesFormat = images.recordset?.map((ele)=>{
-                   return ele?.url;
-                 }) ?? [];
-                 item["images"] = imagesFormat;
-                 return item;
-               })
-            );
+    try {
+      switch (req.params.table.toLowerCase()) {
+        case "products":
+          Result = await new Promise((resolve, reject) => {
+            connection.query("SELECT * FROM products", (error, results) => {
+              if (error) {
+                reject(error);
+              } else {
+                resolve(results);
+              }
+            });
+          });
 
-           res.send(result);
-            break;
-          default: {
-            Result = await pool.request()
-              .query("SELECT * FROM products JOIN image ON products.id = image.prd_id");
-            res.send(Result.recordset);
-          }
+          let result = await Promise.all(
+            Result.map(async (item) => {
+              let images = await new Promise((resolve, reject) => {
+                connection.query("SELECT url FROM image where prd_id = ?", [item.id], (error, results) => {
+                  if (error) {
+                    reject(error);
+                  } else {
+                    resolve(results);
+                  }
+                });
+              });
+
+              item.images = images.map((ele) => ele.url);
+              return item;
+            })
+          );
+
+          res.send(result);
+          break;
+        default: {
+          Result = await new Promise((resolve, reject) => {
+            connection.query("SELECT * FROM products JOIN image ON products.id = image.prd_id", (error, results) => {
+              if (error) {
+                reject(error);
+              } else {
+                resolve(results);
+              }
+            });
+          });
+
+          res.send(Result);
         }
-      } catch (ex) {
-        res.send(ex);
       }
+    } catch (ex) {
+      res.send(ex);
     }
-  );
+
+    connection.end(); // Close the connection after using it
+  }
+);
 
 
 
